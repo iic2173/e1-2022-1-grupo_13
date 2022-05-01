a8e322eb5066d479e4af399c087978f0c6c3174a
Nicolas Venegas <nvenegasu@uc.cl>
2022-05-01 17:05:18 -0400

    feat: avanze en react

---
+++ frontend/src/components/Users/Items.jsx
@@ -0,0 +1,13 @@
+function Items({ currentItems }) {
+    return (
+      <div className="items">
+      {currentItems && currentItems.map((item) => (
+        <div>
+          <h3>Item #{item}</h3>
+        </div>
+      ))}
+        </div>
+    );
+  }
+
+export default Items;
--- frontend/src/views/User/UserList.jsx
+++ frontend/src/views/User/UserList.jsx
@@ -1,36 +1,60 @@
 import { Link } from 'react-router-dom';
+import PaginatedItems from '../../components/Users/UsersPaginate';
+import React, { useState, useEffect } from 'react';
 
-const users = [
-    {
-        id: 1,
-        nickname: 'user1',
-        phone_num: '123456'
-    },
-    {
-        id: 2,
-        nickname: 'user3',
-        phone_num: '123456'
-    },
-    {
-        id: 3,
-        nickname: 'donde esta el 2',
-        phone_num: '123456'
-    }
-];
+
 
 
 const UserList = () => {
-    return(
-        <section className='container'>
-            <Link to='/'>Inicio</Link>
-            <h2>Usuarios</h2>
-            {users.map((user) => (
-                <div key={user.id}>
-                    <Link to={`/users/${user.id}`}>{`${user.nickname}`}</Link>
-                </div>
-            ))}
-        </section>
-    )
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        if (!response.ok) {
+          setError(true);
+          return [];
+        }
+        return response.json();
+      })
+      .then(setUsers)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="container">
+        <h2>Cargando...</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="container">
+      <Link to="/">Inicio</Link>
+      {error ? (
+        <h2>Error</h2>
+      ) : (
+        <>
+          <h2>Users</h2>
+          {users.map(({ id, name }) => (
+            <div key={id}>
+              <Link to={`/users/${id}`}>{name}</Link>
+            </div>
+          ))}
+          <PaginatedItems itemsPerPage={4} items={users} />
+
+        </>
+      )}
+
+    </section>
+  );
 };
+
 
 export default UserList;

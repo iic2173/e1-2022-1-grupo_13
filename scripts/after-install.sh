b26e96dae56f4fa2f05520d719366ffb9edd13e4
SEBASTIAN RAMOS <ramos.sebastian1@uc.cl>
2022-05-21 22:14:05 -0400

    add sudo

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,5 +2,5 @@
 sudo chown -R ubuntu:root /home/ubuntu/e1-2022-1-grupo_13
 cd /home/ubuntu/e1-2022-1-grupo_13
 sudo mv ../.env ./.env
-docker-compose build
-docker-compose up -d
+sudo docker-compose build
+sudo docker-compose up -d

3fc99bf08cd6df0a6ac24bcb4ac938fd016aa175
SEBASTIAN RAMOS <ramos.sebastian1@uc.cl>
2022-05-26 22:17:02 -0400

    cd

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+# cd
 cd /home/ubuntu/e1-2022-1-grupo_13
-sudo docker-compose down
+sudo docker-compose down -v
 sudo cp -r .env ../
 cd ~
 sudo rm -fr /home/ubuntu/e1-2022-1-grupo_13

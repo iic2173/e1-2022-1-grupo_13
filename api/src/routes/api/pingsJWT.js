7753331ac22f084c7807898a3ea027e3ede2d3eb
Nicolas Venegas <nvenegasu@uc.cl>
2022-05-28 19:16:40 -0400

    fix: indexes

--- api/src/routes/api/pingsJWT.js
+++ api/src/routes/api/pingsJWT.js
@@ -39,43 +39,22 @@ router.patch('api.pings.accept', '/:id/accept', async (ctx) => {
     const { ping } = ctx.state;
     try {
         // enviar un body con "status: 1 o 2 " 1 es aceptado 2 es rechazado
-<<<<<<< HEAD
         await ping.update({ status: 1 })
         ctx.body = ping;
     } catch (validationError) {
         ctx.throw(400)
     }
-=======
-        const { response } = ctx.request.body;
-        await ping.update({ status: 1 })
-
-    } catch (validationError) {
-        ctx.throw(400)
-    }
-
->>>>>>> 4a6c814af5977e914261cb40f72cf3ae92ceb955
 })
 
 router.patch('api.pings.reject', '/:id/reject', async (ctx) => {
     
     const { ping } = ctx.state;
     try {
-<<<<<<< HEAD
         await ping.update({ status: 2 })
         ctx.body = ping;
     } catch (validationError) {
         ctx.throw(400)
     }
-=======
-        // enviar un body con "status: 1 o 2 " 1 es aceptado 2 es rechazado
-        const { response } = ctx.request.body;
-        await ping.update({ status: 2 })
-
-    } catch (validationError) {
-        ctx.throw(400)
-    }
-
->>>>>>> 4a6c814af5977e914261cb40f72cf3ae92ceb955
 })
 
 router.get("api.pings.list", "/recieved", async (ctx) => {

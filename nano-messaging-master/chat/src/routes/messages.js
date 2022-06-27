31028573af207f37937bf2a56642e697afa61cba
Nicolas Venegas <nvenegasu@uc.cl>
2022-06-27 00:08:17 -0400

    feat: historial de chat endpoint

--- nano-messaging-master/chat/src/routes/messages.js
+++ nano-messaging-master/chat/src/routes/messages.js
@@ -2,4 +2,16 @@ const jRouter = require('koa-joi-router');
 
 const router = jRouter();
 
+router.route({
+    method: 'get',
+    path: '/history',
+    validate: {},
+    handler: async(ctx) => {
+        const messages = await ctx.orm.Message.findAll();
+        ctx.status = 200;
+        ctx.response.json = messages;
+
+    }
+})
+
 module.exports = router;
